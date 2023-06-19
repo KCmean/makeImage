fe4be8a4907f787128d468d0842079e69c959768
KCmean <kunalmanu2468@gmail.com>
2023-06-19 08:28:22 +0530

    feat: atlas setup and schema

--- server/index.js
+++ server/index.js
@@ -1,23 +1,30 @@
-import express from 'express';
-import * as dotenv from 'dotenv';
-import cors from 'cors';
+import express from "express";
+import * as dotenv from "dotenv";
+import cors from "cors";
+
+import connectDB from "./mongodb/connect.js";
 
 dotenv.config();
 
 const app = express();
 app.use(cors());
-app.use(express.json({limit: '50mb'}));
+app.use(express.json({ limit: "50mb" }));
 
-app.get('/', (req, res) => {
-    res.send('Hello server!');
-})
+app.get("/", (req, res) => {
+  res.send("Hello server!");
+});
 
 const startServer = async () => {
-    // const port = process.env.PORT || 5000;
+  // const port = process.env.PORT || 5000;
+
+  try {
+    connectDB(process.env.MONGO_URL);
     app.listen(8080, () => {
-        console.log(`Server is running on port http://localhost:8080`);
-    })
+      console.log(`Server is running on port http://localhost:8080`);
+    });
+  } catch (e) {
+    console.log(e);
+  }
 };
 
 startServer();
-
